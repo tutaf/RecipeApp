e7257dad1b328a5f1db7c21986cbb083ee62297c
tutaf <andrei.cernisov@isa.utm.md>
2024-04-24 13:37:48 +0300

    aaaaaaaa it works

--- app/src/main/java/com/tutaf/recipeapp/view/Categories.kt
+++
@@ -1,6 +0,0 @@
-package com.tutaf.recipeapp.view
-
-data class Category(
-    val strCategoryThumb: String,
-    val strCategory: String
-)
--- app/src/main/java/com/tutaf/recipeapp/view/CategoriesScreen.kt
+++ app/src/main/java/com/tutaf/recipeapp/view/CategoriesScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -32,26 +33,61 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.tutaf.recipeapp.model.ApiCategory
+import com.tutaf.recipeapp.viewModel.CategoriesViewModel
+import org.koin.androidx.compose.koinViewModel
 
-const val CategoriesScreenDestination= "Categories"
+
+const val categoryScreenArgCategory = "category"
+ const val categoryScreenRouteDefinition = "category/{${categoryScreenArgCategory}}"
+ fun categoryScreenRoute(category: String) = "category/$category"
+
+ @Composable
+ fun CategoryScreen(
+     viewModel: CategoriesViewModel = koinViewModel()
+ ) {
+     val categories by viewModel.categories.collectAsStateWithLifecycle()
+ //    val meals by viewModel.meals.collectAsStateWithLifecycle()
+
+     CategoriesScreenContent(categories)
+
+ }
+
+// @Composable
+// fun CategoryScreenContent(categories: List<ApiCategory>) {
+//
+//     LazyColumn (
+//         modifier = Modifier.fillMaxSize(),
+//         horizontalAlignment = Alignment.CenterHorizontally,
+//         verticalArrangement = Arrangement.spacedBy(16.dp)
+//     ){
+//         items(categories) { category ->
+//             Text(
+//                 text = category.name,
+//             )
+//         }
+//     }
+// }
+
+
+
 val PrimaryBackgroundColor = Color(0xFFC1C1C1)
 val ItemBackgroundColor = Color(0xFFF6F6F6)
+
+//@OptIn(ExperimentalMaterial3Api::class)
+//@Composable
+//fun CategoriesScreen(){
+//
+//    val categories = listOf(myCategory)
+//    CategoriesScreenContent(categories = categories)
+//
+//
+//}
+//
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CategoriesScreen(){
-    val myCategory = Category(
-        strCategoryThumb = "https://www.themealdb.com/images/category/dessert.png",
-        strCategory = "Category Name"
-    )
-    val categories = listOf(myCategory)
-    CategoriesScreenContent(categories = categories)
-
-
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun CategoriesScreenContent(categories: List<Category>) {
+fun CategoriesScreenContent(categories: List<ApiCategory>) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -67,7 +103,7 @@ fun CategoriesScreenContent(categories: List<Category>) {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            items(categories, key = { it.strCategory }) { category ->
+            items(categories, key = { it.name }) { category ->
                 Column(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -85,7 +121,7 @@ fun CategoriesScreenContent(categories: List<Category>) {
                         verticalAlignment = Alignment.CenterVertically
                     ) {
                         Text(
-                            text = category.strCategory,
+                            text = category.name,
                             style = MaterialTheme.typography.bodyLarge,
                             modifier = Modifier.padding(16.dp) // Padding inside the Text
                         )
@@ -97,71 +133,19 @@ fun CategoriesScreenContent(categories: List<Category>) {
     }
 }
 
+
 @Composable
 @Preview(showBackground = true)
 fun CategoriesScreenPreview()  {
     val categories = listOf(
-        Category("soijfd", "Category 1"),
-        Category("soijfd", "Category 2") ,
-                Category("soijfd", "Category 3"),
-    Category("soijfd", "Category 4"),
-    Category("soijfd", "Category 5"),
-    Category("soijfd", "Category 6"),
+        ApiCategory(1, "soijfd", "Category 1", "aaa"),
 
 
 
 
-    )
+
+        )
 
     CategoriesScreenContent(categories = categories)
 }
 
-
-
-// import androidx.compose.foundation.layout.Arrangement
-// import androidx.compose.foundation.layout.fillMaxSize
-// import androidx.compose.foundation.lazy.LazyColumn
-// import androidx.compose.foundation.lazy.items
-// import androidx.compose.material3.Text
-// import androidx.compose.runtime.Composable
-// import androidx.compose.runtime.getValue
-// import androidx.compose.ui.Alignment
-// import androidx.compose.ui.Modifier
-// import androidx.compose.ui.unit.dp
-// import androidx.lifecycle.compose.collectAsStateWithLifecycle
-// import com.tutaf.recipeapp.viewModel.CategoriesViewModel
-// import  androidx.lifecycle.viewmodel.compose.viewModel
-// import com.tutaf.recipeapp.model.ApiCategory
-// import org.koin.androidx.compose.koinViewModel
-
-
-// const val categoryScreenArgCategory = "category"
-// const val categoryScreenRouteDefinition = "category/{${categoryScreenArgCategory}}"
-// fun categoryScreenRoute(category: String) = "category/$category"
-
-// @Composable
-// fun CategoryScreen(
-//     viewModel: CategoriesViewModel = koinViewModel()
-// ) {
-//     val categories by viewModel.categories.collectAsStateWithLifecycle()
-// //    val meals by viewModel.meals.collectAsStateWithLifecycle()
-
-//     CategoryScreenContent(categories)
-
-// }
-
-// @Composable
-// fun CategoryScreenContent(categories: List<ApiCategory>) {
-
-//     LazyColumn (
-//         modifier = Modifier.fillMaxSize(),
-//         horizontalAlignment = Alignment.CenterHorizontally,
-//         verticalArrangement = Arrangement.spacedBy(16.dp)
-//     ){
-//         items(categories) { category ->
-//             Text(
-//                 text = category.name,
-//             )
-//         }
-//     }
-// }
